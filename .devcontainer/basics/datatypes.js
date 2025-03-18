d783e635a3247c5c209085aecb75dd898bf3bf42
AsmitaAdhikary <asmitaadhikary07@gmail.com>
2025-03-18 05:57:25 +0000

    container config

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+/*
+//in this way we cannot use alert
+alert(3+3) //we are using nodejs not browser
+alert("hello") 
+*/
+
+console.log(3+3)
+
+let name="asmita"
+let age=22
+let isloggedin=false
+let temparature=null
+let state;
+
+
+//number -> 2 to the power 53
+//bigint
+//string->""
+//boolean=>true/false
+//null=>standalone value  (empty value)
+//undefined=> not defined
+//symbol=>unique,to find uniqueness, mostly used in react
+
+//object
+
+console.log(typeof "asmita")
+console.log(typeof isloggedin)
+console.log(typeof null) //o/p=> object
+console.log(typeof undefined)
